82bc72558c5b616af1f318af3d87e6c96e537b5f
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-07 12:53:47 -0400

    added circles and colors

--- Week2/Drawing/js/script.js
+++ Week2/Drawing/js/script.js
@@ -19,5 +19,37 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-    background(100,255,100);
+    colorMode(HSB);
+    background(250, 100, 50);
+    colorMode(RGB);
+
+    push();
+    fill(50, 0.0);
+    stroke(255);
+    ellipse(320, 320, 500);
+    pop();
+
+    push();
+    fill('white');
+    noStroke();
+    ellipse(320, 320, 300);
+    pop();
+
+    push();
+    fill(50, 0.0);
+    colorMode(HSB);
+    stroke(250, 100, 50);
+    ellipse(320, 320, 200);
+    pop()
+
+    push();
+    colorMode(HSB);
+    fill(250, 100, 50);
+    ellipse(320, 320, 100);
+    pop();
+
+    push();
+    
+    strokeWeight(2)
+    pop();
 }
