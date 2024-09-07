32f06dfdefed5bd773f85dba8649ad5d50f96cdd
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-06 21:57:43 -0400

    Described code

--- Week2/FunctionCallsAtt2/js/script.js
+++ Week2/FunctionCallsAtt2/js/script.js
@@ -4,30 +4,25 @@
  * 
  * A static representation of a chess board
  * It is square as per the dimensions of a true board
+ * Doesn't actually do anything at the moment
  */
 
 "use strict";
 
-
+/**
+ * Creates the main canvas for the board
+ */
 function setup() {
-    //executes once at the beginning of the program
-    //Press tab before writing
-    //pre-written code, do not need to know how it works
-    //double slash tells computer not to read
-    //CASE SENSITIVE!!!! CMD WILL NOT BE VALID WITH WRONG CAPS!!
-    //width x height in px
-    //semicolon = command finished
-    //tells computer command and brackets tells what to do for command
+    // Draws canvas at given resolution (WxH in px)
     createCanvas(900, 900);
 }
-
+/**
+ * materializes and colors the board, adds all the white squares to yield playing tiles  
+ */
 function draw() {
-    //Once at the beginning of the program
-    // RGB values from 255 to 0
-    // Spaces after the comma
-    //SHOULD HAVE LINE ON LEFT!!! CODE WILL NOT RUN IF NOT
-    //Why background is every frame but canvas is not?
+    // Background color of the board (RGB, Black)
     background(0, 0, 0);
+    // White Squares (Left point x, Left point y, W, H)
     rect(50, 50, 100, 100)
     rect(250, 50, 100, 100)
     rect(450, 50, 100, 100)
@@ -61,3 +56,23 @@ function draw() {
     rect(550, 750, 100, 100)
     rect(750, 750, 100, 100)
 }
+/**
+ * IMPORTANT REMINDERS FOR ME!!
+ *      CASE SENSITIVE, CMD WILL NOT BE VALID WITH WRONG CAPS!!
+ *      SHOULD HAVE LINE ON LEFT!!! CODE WILL NOT RUN IF NOT, SOMETHING IS WRONG
+ * NOTES FOR ME!!!
+ *      FUNCTION SETUP
+ *          executes once at the beginning of the program
+ *          tells computer what to do and brackets tells what to do for command (content)
+ *      FUNCTION DRAW
+ *          executes every frame 
+ * GENERIC NOTES FOR ME!!
+ *      Function calls are pre-written code, do not need to know how it works
+ *      Press tab before writing
+ *      /star star = tells computer not to read, multi line
+ *      // = tells computer not to read, single line
+ *      semicolon = command finished
+ *      always put spaces after the comma
+ * QUESTIONS?
+ *      Why background is every frame but canvas is not?
+ */
