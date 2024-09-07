e7f75ef59211d822911f93f6fc65e20345220e84
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-06 20:11:38 -0400

    Wrote notes and record of errors

--- Week2/FunctionCallsAtt2/js/script.js
+++ Week2/FunctionCallsAtt2/js/script.js
@@ -9,11 +9,22 @@
 
 
 function setup() {
-    //Once at the beginning of the program
+    //executes once at the beginning of the program
+    //Press tab before writing
+    //pre-written code, do not need to know how it works
+    //double slash tells computer not to read
+    //CASE SENSITIVE!!!! CMD WILL NOT BE VALID WITH WRONG CAPS!!
+    //width x height in px
+    //semicolon = command finished
+    //tells computer command and brackets tells what to do for command
     createCanvas(640, 480);
 }
 
 function draw() {
     //Once at the beginning of the program
-    background(255, 100, 100);
+    // RGB values from 255 to 0
+    // Spaces after the comma
+    //SHOULD HAVE LINE ON LEFT!!! CODE WILL NOT RUN IF NOT
+    //Why background is every frame but canvas is not?
+    background(100, 200, 200);
 }
