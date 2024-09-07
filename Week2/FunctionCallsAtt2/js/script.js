c033ca58a3ef112fa1acff46aac854dd310acf94
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-06 21:32:30 -0400

    Squares updated to be chess board + top description changed to fit project

--- Week2/FunctionCallsAtt2/js/script.js
+++ Week2/FunctionCallsAtt2/js/script.js
@@ -1,8 +1,9 @@
 /**
- * Function Calls
+ * The Chess Board
  * Nadia Abdul Aziz
  * 
- * Practice examples
+ * A static representation of a chess board
+ * It is square as per the dimensions of a true board
  */
 
 "use strict";
@@ -17,7 +18,7 @@ function setup() {
     //width x height in px
     //semicolon = command finished
     //tells computer command and brackets tells what to do for command
-    createCanvas(1000, 1000);
+    createCanvas(900, 900);
 }
 
 function draw() {
@@ -26,10 +27,37 @@ function draw() {
     // Spaces after the comma
     //SHOULD HAVE LINE ON LEFT!!! CODE WILL NOT RUN IF NOT
     //Why background is every frame but canvas is not?
-    background(100, 200, 200);
-    rect(400, 400, 200, 200)
-    rect(300, 300, 50, 50)
-    rect(650, 650, 50, 50)
-    rect(650, 300, 50, 50)
-    rect(300, 650, 50, 50)
+    background(0, 0, 0);
+    rect(50, 50, 100, 100)
+    rect(250, 50, 100, 100)
+    rect(450, 50, 100, 100)
+    rect(650, 50, 100, 100)
+    rect(150, 150, 100, 100)
+    rect(350, 150, 100, 100)
+    rect(550, 150, 100, 100)
+    rect(750, 150, 100, 100)
+    rect(50, 250, 100, 100)
+    rect(250, 250, 100, 100)
+    rect(450, 250, 100, 100)
+    rect(650, 250, 100, 100)
+    rect(150, 350, 100, 100)
+    rect(350, 350, 100, 100)
+    rect(550, 350, 100, 100)
+    rect(750, 350, 100, 100)
+    rect(50, 450, 100, 100)
+    rect(250, 450, 100, 100)
+    rect(450, 450, 100, 100)
+    rect(650, 450, 100, 100)
+    rect(150, 550, 100, 100)
+    rect(350, 550, 100, 100)
+    rect(550, 550, 100, 100)
+    rect(750, 550, 100, 100)
+    rect(50, 650, 100, 100)
+    rect(250, 650, 100, 100)
+    rect(450, 650, 100, 100)
+    rect(650, 650, 100, 100)
+    rect(150, 750, 100, 100)
+    rect(350, 750, 100, 100)
+    rect(550, 750, 100, 100)
+    rect(750, 750, 100, 100)
 }
