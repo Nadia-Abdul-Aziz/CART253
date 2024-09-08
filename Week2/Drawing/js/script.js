0d99225562bf484cef3f37a83194b930e0617864
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-08 00:02:12 -0400

    added descriptions

--- Week2/Drawing/js/script.js
+++ Week2/Drawing/js/script.js
@@ -1,14 +1,15 @@
 /**
- * Drawing
+ * Moon Cult
  * Nadia Abdul Aziz
  * 
- * Practicing more functions within draw
+ * Practicing more functions within draw, accidentally created a cult emblem
+ * 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas, square, standard rez
 */
 function setup() {
     createCanvas(640, 640);
@@ -16,31 +17,42 @@ function setup() {
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Displays the ellipses that make up the piece
 */
 function draw() {
+    //Change color to hue, sat and brightness
     colorMode(HSB);
+    //dark blue
     background(250, 100, 50);
+    //switching back to rgb in master project (whole project)
     colorMode(RGB);
 
     // Outermost ring
     push();
+    //transparent
     fill(50, 0.0);
+    //white border
     stroke(255);
+    //circle in center
     ellipse(320, 320, 500);
     pop();
 
     // largest white circle
     push();
+    //white color
     fill('white');
+    //no border
     noStroke();
+    //circle
     ellipse(320, 320, 300);
     pop();
 
     // Inner white circle (with blue outline)
     push();
     fill(50, 0.0);
+    //change color to hue, sat, bright
     colorMode(HSB);
+    //dark blue border
     stroke(250, 100, 50);
     ellipse(320, 320, 200);
     pop()
@@ -48,6 +60,7 @@ function draw() {
     // smallest blue circle (functions as background for moon design)
     push();
     colorMode(HSB);
+    //dark blue color
     fill(250, 100, 50);
     ellipse(320, 320, 100);
     pop();
@@ -56,6 +69,7 @@ function draw() {
     push();
     colorMode(HSB);
     fill(250, 100, 50);
+    //thickenned border to 7px
     strokeWeight(7);
     stroke(250, 100, 50);
     ellipse(450, 220, 100);
@@ -106,6 +120,7 @@ function draw() {
     ellipse(320, 320, 80);
     pop();
 
+    //corner frame for moon
     push();
     colorMode(HSB);
     fill(50, 0.0);
@@ -114,12 +129,14 @@ function draw() {
     ellipse(450, 220, 70);
     pop();
 
+    //corner circle to create moon shape
     push();
     noStroke();
     fill('white');
     ellipse(440, 215, 55);
     pop();
 
+   //corner accents to frame the piece
     push();
     fill(50, 0.0);
     stroke('white');
