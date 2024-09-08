c960c65c7529ebbd8d6fb8e8ef1b02de3b4c0276
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-07 23:48:05 -0400

    Moon Cult drawn

--- Week2/Drawing/js/script.js
+++ Week2/Drawing/js/script.js
@@ -23,18 +23,21 @@ function draw() {
     background(250, 100, 50);
     colorMode(RGB);
 
+    // Outermost ring
     push();
     fill(50, 0.0);
     stroke(255);
     ellipse(320, 320, 500);
     pop();
 
+    // largest white circle
     push();
     fill('white');
     noStroke();
     ellipse(320, 320, 300);
     pop();
 
+    // Inner white circle (with blue outline)
     push();
     fill(50, 0.0);
     colorMode(HSB);
@@ -42,14 +45,85 @@ function draw() {
     ellipse(320, 320, 200);
     pop()
 
+    // smallest blue circle (functions as background for moon design)
     push();
     colorMode(HSB);
     fill(250, 100, 50);
     ellipse(320, 320, 100);
     pop();
 
+    // Shading for corner ring
     push();
+    colorMode(HSB);
+    fill(250, 100, 50);
+    strokeWeight(7);
+    stroke(250, 100, 50);
+    ellipse(450, 220, 100);
+    pop();
     
-    strokeWeight(2)
+    // Corner ring 
+    push();
+    colorMode(HSB);
+    fill(250, 100, 50);
+    strokeWeight(2.5);
+    stroke('white');
+    ellipse(450, 220, 100);
+    pop();
+
+    push();
+    fill(50, 0.0);
+    stroke(255);
+    ellipse(450, 220, 88);
+    pop();
+
+    // Innermost white circle (cut by blue circle to create moon shape)
+    push();
+    noStroke();
+    fill(255);
+    ellipse(320, 320, 60);
+    pop();
+
+    //Innermost blue circle (to cut through the white circle)
+    push();
+    colorMode(HSB);
+    noStroke();
+    fill(250, 100, 50);
+    ellipse(310, 315, 60);
+    pop();
+
+    // Outer frame for moon
+    push();
+    fill(50, 0.0);
+    stroke(255);
+    strokeWeight(2);
+    ellipse(320, 320, 90);
+    pop();
+
+    // Inner frame for moon
+    push();
+    fill(50, 0.0);
+    stroke(255);
+    ellipse(320, 320, 80);
+    pop();
+
+    push();
+    colorMode(HSB);
+    fill(50, 0.0);
+    stroke(255);
+    strokeWeight(7);
+    ellipse(450, 220, 70);
+    pop();
+
+    push();
+    noStroke();
+    fill('white');
+    ellipse(440, 215, 55);
+    pop();
+
+    push();
+    fill(50, 0.0);
+    stroke('white');
+    strokeWeight(7);
+    ellipse(320, 320, 800)
     pop();
 }
