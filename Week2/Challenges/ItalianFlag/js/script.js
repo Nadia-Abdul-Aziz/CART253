ba64d158543319c5a07069f160f9b8897a5228a1
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-12 11:48:13 -0400

    Added template files for italy & chain + finished Italy

---
+++ Week2/Challenges/ItalianFlag/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * Italian Flag
+ * Nadia Abdul Aziz
+ * 
+ * An attempt to make the italian flag
+ */
+
+"use strict";
+
+/**
+ * Creates the canvas of the flag
+*/
+function setup() {
+    createCanvas(800, 800);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(100, 150, 255);
+    fill(0, 255, 0);
+    noStroke();
+    rect(100, 200, 200, 400);
+    noStroke();
+    fill(255, 255, 255);
+    rect(300, 200, 200, 400);
+    fill(255, 0, 0);
+    noStroke();
+    rect(500, 200, 200, 400);
+
+}
