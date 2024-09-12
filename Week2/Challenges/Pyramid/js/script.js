d8f6f1b8b6b81c7a4e482d8f7494abd8c0960b26
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-12 13:28:12 -0400

    some of pyramid done, not complete

---
+++ Week2/Challenges/Pyramid/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * Pyramid
+ * Nadia Abdul Aziz
+ * 
+ * Attempt at the pyramid from challenges
+ */
+
+"use strict";
+
+/**
+ * Draw canvas
+*/
+function setup() {
+    createCanvas(800, 800);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(100, 200, 255);
+
+    fill(255, 255, 0);
+    noStroke();
+    ellipse(600, 900, 800, 800);
+
+    ellipse(100, 700, 800, 800);
+
+    triangle(300, 300, 200,)
+
+
+}
