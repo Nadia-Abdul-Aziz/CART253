3c397b2a572ff4d0658b2028be4acbc362919676
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-19 15:26:07 -0400

    working on shaking over time, finished all the rest

--- Week3/Challenge/js/script.js
+++ Week3/Challenge/js/script.js
@@ -8,6 +8,11 @@
 "use strict";
 
 // Our friend Mr. Furious
+let back = {
+    r: 160,
+    g: 180,
+    b: 200,
+};
 let mrFurious = {
     // Position and size
     x: 200,
@@ -19,6 +24,14 @@ let mrFurious = {
         g: 225,
         b: 225,
     }
+};
+
+let bird = {
+    x: 0,
+    y: 100,
+    size: 10,
+    speed: 0.25,
+    fill: 'blue',
 };
 
 /**
@@ -33,13 +46,48 @@ function setup() {
  */
 function draw() {
 
+    // mr furious turning red
+    mrFurious.fill.g += -1;
+    mrFurious.fill.g = constrain(mrFurious.fill.g, 0, 255);
+    mrFurious.fill.b += -1;
+    mrFurious.fill.b = constrain(mrFurious.fill.b, 0, 255);
 
-    background(160, 180, 200);
+    // background turning to night
+    back.r += -2;
+    back.r = constrain(back.r, 0, 255);
+    back.g += -2;
+    back.g = constrain(back.g, 0, 255);
+    back.b += -2;
+    back.b = constrain(back.b, 0, 255);
+
+    // move borb
+
+    bird.x += 2
+    bird.y += 1
+
+    //harlem shake
+
+    const x = mrFurious.x + random(-3, 3);
+    const y = mrFurious.y + random(-3, 3);
+
+    //shake more over time
+
+    x = + random(-3, 3);
+    y = + random(-3, 3);
+
+    background(back.r, back.g, back.b);
 
     // Draw Mr. Furious as a coloured circle
     push();
     noStroke();
     fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
-    ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+    ellipse(x, y, mrFurious.size);
     pop();
+
+    // borb
+    push();
+    noStroke();
+    fill(bird.fill);
+    rect(bird.x, bird.y, bird.size);
+
 }
