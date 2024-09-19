180dfbdb31f9442a2c15b9fa95dd3ee5e21d05af
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-19 02:21:42 -0400

    working on movement, made bird

--- Week3/Movement/js/script.js
+++ Week3/Movement/js/script.js
@@ -7,11 +7,17 @@
 
 "use strict";
 
+let bird = {
+    x: 120,
+    y: 480,
+    size: 50,
+};
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(640, 480)
 }
 
 
@@ -19,5 +25,15 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background('black');
 
+    // move bird
+    bird.x = bird.x + 1;
+    bird.y = bird.y - 2;
+
+    push();
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(bird.x, bird.y, bird.size);
+    pop();
 }
