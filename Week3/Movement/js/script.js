b0c835f58532018c131a1baa4930a19bcf120d04
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-19 12:07:37 -0400

    Finished bird, too slow????

--- Week3/Movement/js/script.js
+++ Week3/Movement/js/script.js
@@ -11,7 +11,25 @@ let bird = {
     x: 120,
     y: 480,
     size: 50,
+    velocity: {
+        x: 0,
+        y: 0,
+    },
+    minVelocity: {
+        x: -3,
+        y: -2,
+    },
+    maxVelocity: {
+        x: 3,
+        y: 2,
+    },
+    accel: {
+        x: 0.025,
+        y: -0.05,
+    },
 };
+
+
 
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
@@ -27,9 +45,19 @@ function setup() {
 function draw() {
     background('black');
 
+    // speed limit
+
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+
+    // acceleration 
+
+    bird.velocity.x += + bird.accel.x;
+    bird.velocity.y += + bird.accel.y;
+
     // move bird
-    bird.x = bird.x + 1;
-    bird.y = bird.y - 2;
+    bird.x += + bird.velocity.x;
+    bird.y += + bird.velocity.y;
 
     push();
     fill(255, 0, 0);
