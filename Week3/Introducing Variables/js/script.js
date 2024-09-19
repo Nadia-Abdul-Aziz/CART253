9aeba7a648877740be84d95682d00e46ac8b45d2
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-19 00:03:59 -0400

    Rainbow squishy

--- Week3/Introducing Variables/js/script.js
+++ Week3/Introducing Variables/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * Nadia
+ * Variable explanation
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * WTF is a variable and how to use it
  */
 
 "use strict";
@@ -12,7 +11,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(640, 480);
 }
 
 
@@ -20,5 +19,12 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
 
+    //circle
+    push();
+    fill(mouseX, mouseY, 0);
+    noStroke();
+    ellipse(mouseX, mouseY, mouseX, mouseY);
+    pop();
 }
