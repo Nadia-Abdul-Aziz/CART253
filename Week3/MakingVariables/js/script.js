3518f2cefc2ce4a1bbf78a0f5411984d2fb50c81
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-09-19 00:11:38 -0400

    hole in piece of cheese

--- Week3/MakingVariables/js/script.js
+++ Week3/MakingVariables/js/script.js
@@ -11,7 +11,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(480, 480);
 }
 
 
@@ -19,5 +19,10 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(255, 255, 0);
 
+    push();
+    noStroke();
+    fill(255);
+    ellipse(400, 100, 100, 100)
 }
