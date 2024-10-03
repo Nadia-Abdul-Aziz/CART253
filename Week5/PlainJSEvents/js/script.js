b802e07f2e071cf9eec87a7b49c4253761d450d8
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-10-03 12:52:16 -0400

    almost done plainjs experiment

--- Week5/PlainJSEvents/js/script.js
+++ Week5/PlainJSEvents/js/script.js
@@ -1,24 +1,37 @@
 /**
- * Title of Project
- * Author Name
+ * Plain JS events
+ * Nadia
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * event handling without p5
  */
 
 "use strict";
 
+//info abt bg fills
+const bg = {
+    fill: "black",
+    fills: {
+        black: "black",
+        white: "white"
+    }
+}
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * create canvas
 */
 function setup() {
+    createCanvas(400,400);
 
+    window.addEventListener("keydown", changeBg);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * draws background
 */
 function draw() {
+    background(bg.fill);
+}
 
+function changeBg(){
+    
 }
