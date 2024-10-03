7d56bb4f02498a4269a9042202fbdcb869133ba7
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-10-03 01:54:08 -0400

    Most of background completed

--- ArtJam1/SoccerGame/js/script.js
+++ ArtJam1/SoccerGame/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * SoccerGame
+ * Nadia Abdul Aziz
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -11,14 +11,61 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
+
+
 function setup() {
-
+    createCanvas(900, 650);
+    rectMode(CENTER);
 }
-
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * 
 */
-function draw() {
 
+function draw() {
+    drawGrass();
+
+    push();
+    stroke(255, 255, 255);
+    strokeWeight(5);
+    fill(50, 0);
+
+    //Frame outline
+    rect(width / 2, height / 2, width - 20, height - 20);
+
+    //Middle of the field circle
+    const arcSizeX = 200;
+    const arcSizeY = 200;
+    arc(width / 2, height - 15, arcSizeX, arcSizeY, PI, TWO_PI);
+
+    //Goal outline
+    const rectPositionY = 85;
+    const rectWidth = 400;
+    const rectHeight = 150;
+    push();
+    fill("green");
+    rect(width / 2, rectPositionY, rectWidth, rectHeight);
+    pop();
+
+    //Penalty arc
+    const penaltyPositionY = rectPositionY + rectHeight / 2;
+    const penaltySizeX = 200;
+    const penaltySizeY = 200;
+    arc(width / 2, penaltyPositionY, penaltySizeX, penaltySizeY, 0, PI);
+    pop();
 }
+
+
+//Draws full field of grass
+function drawGrass() {
+    drawBg();
+};
+
+function drawLine() {
+
+};
+
+//Draws background
+function drawBg() {
+    const back = "green";
+    background(back);
+};
