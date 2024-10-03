4f861fbc872083131b467fc88fb9505a63a0c573
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-10-03 02:29:41 -0400

    Background field finished

--- ArtJam1/SoccerGame/js/script.js
+++ ArtJam1/SoccerGame/js/script.js
@@ -2,66 +2,33 @@
  * SoccerGame
  * Nadia Abdul Aziz
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Tentative soccer type game
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates Canvas & dictates rectangle mode
 */
-
-
 function setup() {
     createCanvas(900, 650);
     rectMode(CENTER);
 }
-/**
- * 
-*/
 
+/**
+ * Creates the background visuals, paddle, goal and ball
+*/
 function draw() {
     drawGrass();
-
-    push();
-    stroke(255, 255, 255);
-    strokeWeight(5);
-    fill(50, 0);
-
-    //Frame outline
-    rect(width / 2, height / 2, width - 20, height - 20);
-
-    //Middle of the field circle
-    const arcSizeX = 200;
-    const arcSizeY = 200;
-    arc(width / 2, height - 15, arcSizeX, arcSizeY, PI, TWO_PI);
-
-    //Goal outline
-    const rectPositionY = 85;
-    const rectWidth = 400;
-    const rectHeight = 150;
-    push();
-    fill("green");
-    rect(width / 2, rectPositionY, rectWidth, rectHeight);
-    pop();
-
-    //Penalty arc
-    const penaltyPositionY = rectPositionY + rectHeight / 2;
-    const penaltySizeX = 200;
-    const penaltySizeY = 200;
-    arc(width / 2, penaltyPositionY, penaltySizeX, penaltySizeY, 0, PI);
-    pop();
 }
 
+
+//////////ALL FUNCTIONS FOR THE BACKGROUND FIELD
 
 //Draws full field of grass
 function drawGrass() {
     drawBg();
-};
-
-function drawLine() {
-
+    drawLine();
 };
 
 //Draws background
@@ -69,3 +36,47 @@ function drawBg() {
     const back = "green";
     background(back);
 };
+
+//Draws field lines
+function drawLine() {
+
+    push();
+    stroke(255, 255, 255);
+    strokeWeight(5);
+    fill(50, 0);
+
+    drawMiddleArc();
+    drawFrame();
+    drawGoalOutline();
+
+    pop();
+};
+
+function drawFrame() {
+    //Frame outline
+    rect(width / 2, height / 2, width - 20, height - 20);
+};
+
+function drawMiddleArc() {
+    //Middle of the field circle
+    const arcSizeX = 200;
+    const arcSizeY = 200;
+    arc(width / 2, height - 15, arcSizeX, arcSizeY, PI, TWO_PI);
+};
+
+function drawGoalOutline() {
+    //Goal outline
+    const rectPositionY = 85;
+    const rectWidth = 400;
+    const rectHeight = 150;
+    rect(width / 2, rectPositionY, rectWidth, rectHeight);
+
+    //Penalty arc
+    const penaltyPositionY = rectPositionY + rectHeight / 2;
+    const penaltySizeX = 200;
+    const penaltySizeY = 200;
+    arc(width / 2, penaltyPositionY, penaltySizeX, penaltySizeY, 0, PI);
+    pop();
+};
+
+
