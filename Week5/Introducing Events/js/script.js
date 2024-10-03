cc2d2f72d1c4d7b65ad705a5358c3e7e7a062593
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2024-10-03 11:28:37 -0400

    example code for understanding events

--- Week5/Introducing Events/js/script.js
+++ Week5/Introducing Events/js/script.js
@@ -1,9 +1,7 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * intro events
+ * nadia
+ * learning how time works
  */
 
 "use strict";
@@ -12,7 +10,8 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(400, 400);
+    background(0);
 }
 
 
@@ -21,4 +20,12 @@ function setup() {
 */
 function draw() {
 
+}
+
+function mousePressed() {
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(mouseX, mouseY, 50);
+    pop();
 }
